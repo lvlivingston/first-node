c38a4c431d5b470d8132bb56fe60fcf06e93204c
lvlivingston <lvlivingston@gmail.com>
2023-10-16 15:47:17 -0500

    finished lesson

--- main.js
+++ main.js
@@ -17,8 +17,8 @@
 // const daysOfWeek = require('./days-of-week')[1];
 // console.log(daysOfWeek);
 
-const daysOfWeek = require('./days-of-week');
-const day = daysOfWeek.getWeekday(5);
+// const daysOfWeek = require('./days-of-week');
+// const day = daysOfWeek.getWeekday(5);
 
 // console.log(daysOfWeek);
 // console.log(day);
@@ -26,5 +26,18 @@ const day = daysOfWeek.getWeekday(5);
 // node won't see exports, bc node only knows about module.exports
 // console.log(module.exports === exports);
 
-console.log(global);
+// console.log(global);
 
+// const random = require('./random');
+// for (let i = 0; i < 10; i++) {
+//   console.log( random(1, 2) );
+// }
+
+// const random = require('./random');
+// for (let i = 0; i < 10; i++) {
+//   console.log( random(100, 200) );
+// }
+
+const circle = require('./circle');
+ console.log( circle.area(50) );  // 7853.98...
+ console.log( circle.circumference(75) );  // 471.23...
